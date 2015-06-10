49a45925315ee3fe32269892c07e9df74cb7d6f3
sena124 <senamail@me.com>
2015-06-10 18:43:31 +0900

    chenge param

--- 20150610/9_fftExample/src/ofApp.cpp
+++ 20150610/9_fftExample/src/ofApp.cpp
@@ -2,22 +2,22 @@
 
 
 //--------------------------------------------------------------
-void ofApp::setup(){	 
-
-	// load in sounds:
+void ofApp::setup(){
+    
+    // load in sounds:
     perfume.loadSound("sounds/Perfume_globalsite_sound.wav");
-
-	// the fft needs to be smoothed out, so we create an array of floats
-	// for that purpose:
-	fftSmoothed = new float[8192];
-	for (int i = 0; i < 8192; i++){
-		fftSmoothed[i] = 0;
-	}
-	
-	nBandsToGet = 128;
+    
+    // the fft needs to be smoothed out, so we create an array of floats
+    // for that purpose:
+    fftSmoothed = new float[8192];
+    for (int i = 0; i < 8192; i++){
+        fftSmoothed[i] = 0;
+    }
+    
+    nBandsToGet = 128;
     perfume.play();
     
-
+    
     int bufferSize = 256;
     left.assign(bufferSize, 0.0);
     right.assign(bufferSize, 0.0);
@@ -32,40 +32,40 @@ void ofApp::setup(){
         fftSmoothed[i] = 0;
     }
     nBandsToGet = 128;
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     
     ofBackground(80,80,20);
-	
+    
     ofSoundUpdate();
-
+    
     
     ofSoundStreamStart();
-
-
-	float * val = ofSoundGetSpectrum(nBandsToGet);		// request 128 values for fft
-	for (int i = 0;i < nBandsToGet; i++){
-		
-		// let the smoothed calue sink to zero:
-		fftSmoothed[i] *= 0.96f;
-		
-		// take the max, either the smoothed or the incoming:
-		if (fftSmoothed[i] < val[i]) fftSmoothed[i] = val[i];
-		
-	}
-
-
+    
+    
+    float * val = ofSoundGetSpectrum(nBandsToGet);		// request 128 values for fft
+    for (int i = 0;i < nBandsToGet; i++){
+        
+        // let the smoothed calue sink to zero:
+        fftSmoothed[i] *= 0.96f;
+        
+        // take the max, either the smoothed or the incoming:
+        if (fftSmoothed[i] < val[i]) fftSmoothed[i] = val[i];
+        
+    }
+    
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     
     for (int i = 0 ;i<nBandsToGet; i++) {
-//        ofSetColor(fftSmoothed[i]*20000,fftSmoothed[i]*20000,fftSmoothed[i]*20000);
-                ofSetColor(0,fftSmoothed[i]*600,fftSmoothed[i]*20);
+        //        ofSetColor(fftSmoothed[i]*20000,fftSmoothed[i]*20000,fftSmoothed[i]*20000);
+        ofSetColor(0,fftSmoothed[i]*600,fftSmoothed[i]*20);
         ofRect(0,ofGetHeight()-10*(i+1),ofGetWidth(),ofGetHeight()-10*(i));;
     }
 }
@@ -102,46 +102,46 @@ void ofApp::audioIn(float * input, int bufferSize, int nChannels){
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed  (int key){ 
+void ofApp::keyPressed  (int key){
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){ 
-	
+void ofApp::keyReleased(int key){
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-	
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
- 
+
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
 
